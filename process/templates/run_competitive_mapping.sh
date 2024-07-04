07ad962c3d5fe6dd2655f8f37af3b31edb55edb2
Matthew Colpus <mpcolpus@gmail.com>
2024-07-04 13:31:53 +0000

    fix: use -o instead of >

--- process/templates/run_competitive_mapping.sh
+++ process/templates/run_competitive_mapping.sh
@@ -17,8 +17,8 @@ samtools sort -@ $task.cpus -o sorted_alignments.bam alignments.sam 2>>${competi
 
 # Generate a TSV file summarising coverage against each reference genome
 # can use --excl-flags 1540 to include secondary alignments
-samtools coverage sorted_alignments.bam > coverage.tsv 2>>${competitive_mapping_error}
-samtools coverage --excl-flags 1540 sorted_alignments.bam > coverage.secondary.tsv 2>>${competitive_mapping_error}
+samtools coverage sorted_alignments.bam -o coverage.tsv 2>>${competitive_mapping_error}
+samtools coverage --excl-flags 1540 sorted_alignments.bam -o coverage.secondary.tsv 2>>${competitive_mapping_error}
 
 # Index alignments
 samtools index sorted_alignments.bam index.bai 2>>${competitive_mapping_error}
