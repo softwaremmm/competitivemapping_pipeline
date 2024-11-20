2450e8f78efd4697e2acf1429444c425e06ae4ee
Matthew Colpus <mpcolpus@gmail.com>
2024-11-20 14:55:34 +0000

    fix: add test for sylph based comp mapping

--- tests/test_competitive_mapping.py
+++ tests/test_competitive_mapping.py
@@ -53,3 +53,49 @@ def test_cli_entry_point_manifest(
             check_length(output_root + "reads_2.fastq.gz", 9648)
         case "tb_ont":
             check_length(output_root + "reads.fastq.gz", 1042)
+
+
+def test_cli_entry_point_sylph(samples, GTDB_db, test_outputs_dir, mocker):
+    output_root = os.path.join(test_outputs_dir, "cm_sylph", samples["sample"] + ".")
+    os.makedirs(os.path.join(test_outputs_dir, "cm_sylph"), exist_ok=True)
+
+    seq_platform = "ont" if len(samples["reads"]) == 1 else "illumina"
+
+    args = [
+        "competitive_mapping",
+        "sylph",
+        "--reads",
+        " ".join(samples["reads"]),
+        "--seq_platform",
+        seq_platform,
+        "--ref_for_fastq",
+        "gtdb_genomes_reps_r220/database/GCF/000/195/955/GCF_000195955.2_genomic.fna.gz",
+        "--sylph_report",
+        samples["sylph_report"],
+        "--genomes",
+        GTDB_db,
+        "--output_root",
+        output_root,
+    ]
+
+    mocker.patch(
+        "sys.argv",
+        args,
+    )
+
+    competitive_mapping.cli_entry_point()
+
+    check_file(samples["sylph_species_comparison"], output_root + "species_comparison.json")
+
+    def check_length(path, length):
+        assert len(list(SeqIO.parse(gzopen(path, "rt"), format="fastq"))) == length
+
+    match samples["sample"]:
+        case "chloro_10k":
+            check_length(output_root + "reads_1.fastq.gz", 14)
+            check_length(output_root + "reads_2.fastq.gz", 14)
+        case "tb_10k":
+            check_length(output_root + "reads_1.fastq.gz", 9940)
+            check_length(output_root + "reads_2.fastq.gz", 9940)
+        case "tb_ont":
+            check_length(output_root + "reads.fastq.gz", 1044)
